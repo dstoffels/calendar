161a7e0430a7b54d4a9bd157e549ddd062a02382
Lizzy <Lizzy@Lizzys-MacBook-Air.local>
2022-01-14 11:37:53 -0500

    Created logic for display_header().

--- calendar.py
+++ calendar.py
@@ -1,15 +1,25 @@
+from constants import DAYS_OF_WEEK
+
 class Calendar:
   def __init__(self):
-    self.days_of_the_week = []
+    self.days_of_the_week = ''
     self.starting_day = ''
     self.days_in_month = 0
-    self.month_name = ''
-    self.year = 0
+    self.month_name = 'January'
+    self.year = 2022
     self.num_rows = 6
     self.num_col = 7
   
   def display_header(self):
-    pass
+    print(f'{self.month_name}, {self.year}')
+    for day in DAYS_OF_WEEK:
+      self.days_of_the_week += day + "   "
+    print(self.days_of_the_week)
 
   def display_dates(self):
     pass
+
+  def input_month_and_year(self):
+    pass
+
+Calendar().display_header()
