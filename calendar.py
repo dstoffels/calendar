3165a578543911fa3dc7e4d7ca952db729762920
dstoffels <dan.stoffels@gmail.com>
2022-01-14 10:11:41 -0600

    Implemented calendar class & constants.py

--- calendar.py
+++ calendar.py
@@ -1,4 +1,15 @@
 class Calendar:
   def __init__(self):
+    self.days_of_the_week = []
+    self.starting_day = ''
+    self.days_in_month = 0
+    self.month_name = ''
+    self.year = 0
+    self.num_rows = 6
+    self.num_col = 7
+  
+  def display_header(self):
     pass
-  pass
+
+  def display_dates(self):
+    pass
---
+++ constants.py
@@ -0,0 +1,9 @@
+MON = 'Monday'
+TUE = 'Tuesday'
+WED = 'Wednesday'
+THU = 'Thursday'
+FRI = 'Friday'
+SAT = 'Saturday'
+SUN = 'Sunday'
+
+DAYS_OF_WEEK = [MON,TUE,WED,THU,FRI,SAT,SUN]
